e7ed88857fe14cf238a42d8eec0db1b4a3c3da91
Tejada-Joyce <joyce_105_3@hotmail.com>
2019-03-22 15:40:39 -0500

    use session to store data

--- src/main/java/System/Login.java
+++ src/main/java/System/Login.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
 /**
@@ -37,9 +38,18 @@ public class Login extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		String email = request.getParameter("email");
-		String password = request.getParameter("password");
-		//AdminDAO.validate(,)
-		request.getRequestDispatcher("index.jsp").forward(request, response);
+    String password = request.getParameter("password");
+    
+		if (false) {
+      HttpSession session=request.getSession();  
+      session.setAttribute("session", email+password);  
+		  response.sendRedirect("index.jsp");
+		} else {
+      request.setAttribute("error", "INVALID CREDENTIALS");
+      request.getRequestDispatcher("index.jsp").forward(request, response);
+      
+		}
+		
 	}
 
 }
